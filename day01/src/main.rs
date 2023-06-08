2cc9380be424f04cbe531c33f1eeadc359dc250f
Robert Buchberger <robert@buchberger.cc>
2023-06-08 14:22:47 +0200

    Day 1, part 2 complete
    Refactored part 1's answer to avoid duplicating work

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,17 +1,23 @@
 fn main() {
     let text = std::fs::read_to_string("input").unwrap();
 
-    let biggest_elf = text
-        .lines()
-        .collect::<Vec<&str>>()
+    let lines = text.lines().collect::<Vec<&str>>();
+
+    let mut elves = lines
         .rsplit(|e| *e == "")
         .map(|e| {
             e.into_iter()
                 .map(|i| i.parse::<isize>().unwrap())
                 .sum::<isize>()
         })
-        .max()
-        .unwrap();
+        .collect::<Vec<isize>>();
 
+    elves.sort();
+    elves.reverse();
+
+    let biggest_elf = elves.first().unwrap();
     println!("{}", biggest_elf);
+
+    let top_three = elves.iter().take(3).sum::<isize>();
+    println!("{}", top_three);
 }
