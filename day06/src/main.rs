6955a392cd6c1cc0358bb4f195460cfb69e266b5
Robert Buchberger <robert@buchberger.cc>
2023-06-14 10:39:47 +0200

    Day 6, part 2 complete

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 const _EXAMPLE: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"; // marker is 7th char
+const WINDOW_SIZE: usize = 14;
 
 fn main() {
     let input = fs::read_to_string("input")
@@ -8,13 +9,13 @@ fn main() {
         .chars()
         .collect::<Vec<_>>();
 
-    let marker = input.windows(4).enumerate().find(|(_, window)| {
+    let marker = input.windows(WINDOW_SIZE).enumerate().find(|(_, window)| {
         // dedup only works on consecutive dupes, so needs to be sorted first
         let mut window = window.to_vec();
         window.sort();
         window.dedup();
-        window.len() == 4
+        window.len() == WINDOW_SIZE
     });
 
-    println!("{:?}", marker.unwrap().0 + 4);
+    println!("{:?}", marker.unwrap().0 + WINDOW_SIZE);
 }
