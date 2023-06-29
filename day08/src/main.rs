6e5b299b8578c80a8b5305fe77633ded9c565b0f
Robert Buchberger <robert@buchberger.cc>
2023-06-29 15:34:05 +0200

    Day 8, part 2 complete

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -44,5 +44,47 @@ fn main() {
         })
         .sum::<usize>();
 
+    let max_scenic_score = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(row_index, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(col_index, tree)| {
+                    let ( left, [_, right @ ..]) = row.split_at(col_index) else {
+                        panic!("split_at failed");
+                    };
+
+                    let col = grid.iter().map(|r| r[col_index]).collect::<Vec<_>>();
+                    let ( up, [_, down @ ..]) = col
+                        .split_at(row_index) else {
+                            panic!("split_at failed");
+                        };
+
+                    let mut left = left.to_owned();
+                    left.reverse();
+                    let mut up = up.to_owned();
+                    up.reverse();
+
+                    let right: Vec<_> = right.into();
+                    let down: Vec<_> = down.into();
+
+                    [left, right, up, down]
+                        .iter()
+                        .map(|dir| {
+                            if let Some(result) = dir.iter().position(|&t| t >= *tree) {
+                                result + 1
+                            } else {
+                                dir.len()
+                            }
+                        })
+                        .product::<usize>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .max()
+        .unwrap();
+
     dbg!(vis_count);
+    dbg!(max_scenic_score);
 }
