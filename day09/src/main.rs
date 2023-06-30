2ed7d514c7b10d7befc37839b2398781ca6f969c
Robert Buchberger <robert@buchberger.cc>
2023-06-30 11:40:29 +0200

    Day 9, part 2 complete

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -3,21 +3,21 @@ use std::collections::HashSet;
 use std::fs;
 
 const _EXAMPLE: &str = "
-R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
 ";
 
 fn main() {
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
+    let mut rope = vec![(0, 0); 10];
+
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
-    visited.insert(tail);
+    visited.insert(*rope.last().unwrap());
 
     // let text = _EXAMPLE;
     let text = fs::read_to_string("input").unwrap();
@@ -28,28 +28,33 @@ fn main() {
 
         for _ in 0..distance {
             match direction {
-                "R" => head.0 += 1,
-                "L" => head.0 -= 1,
-                "U" => head.1 += 1,
-                "D" => head.1 -= 1,
+                "R" => rope[0].0 += 1,
+                "L" => rope[0].0 -= 1,
+                "U" => rope[0].1 += 1,
+                "D" => rope[0].1 -= 1,
                 _ => panic!("Unknown direction: {}", direction),
             }
 
-            if (head.0 - tail.0).abs() > 1 || (head.1 - tail.1).abs() > 1 {
-                tail.0 += match head.0.cmp(&tail.0) {
-                    Ordering::Less => -1,
-                    Ordering::Equal => 0,
-                    Ordering::Greater => 1,
-                };
+            for i in 1..rope.len() {
+                let leader = rope[i - 1];
+                let mut follower = &mut rope[i];
 
-                tail.1 += match head.1.cmp(&tail.1) {
-                    Ordering::Less => -1,
-                    Ordering::Equal => 0,
-                    Ordering::Greater => 1,
-                };
+                if (leader.0 - follower.0).abs() > 1 || (leader.1 - follower.1).abs() > 1 {
+                    follower.0 += match leader.0.cmp(&follower.0) {
+                        Ordering::Less => -1,
+                        Ordering::Equal => 0,
+                        Ordering::Greater => 1,
+                    };
+
+                    follower.1 += match leader.1.cmp(&follower.1) {
+                        Ordering::Less => -1,
+                        Ordering::Equal => 0,
+                        Ordering::Greater => 1,
+                    };
+                }
             }
 
-            visited.insert(tail);
+            visited.insert(*rope.last().unwrap());
         }
     }
 
