8a8ea2f3cf7466c3082761e624fc92e0bafe594d
Robert Buchberger <robert@buchberger.cc>
2023-07-03 11:45:31 +0200

    Day 10, part 2 complete

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,31 +1,48 @@
 use std::fs;
 
-fn next_cycle(register: isize, cycle_nr: &mut isize, results: &mut Vec<isize>) {
-    if (*cycle_nr - 20) % 40 == 0 {
-        results.push(register * *cycle_nr);
-    }
+const SCREEN_WIDTH: isize = 40;
+const SCREEN_HEIGHT: isize = 6;
+
+fn next_cycle(register: isize, cycle_nr: &mut isize, pickles: &mut Vec<Vec<char>>) {
+    let row = *cycle_nr / SCREEN_WIDTH;
+    let col = *cycle_nr % SCREEN_WIDTH;
+
+    let pickle = if (register - col).abs() < 2 {
+        '█'
+    } else {
+        ' '
+    };
+
+    pickles[row as usize].push(pickle);
 
     *cycle_nr += 1;
 }
 
 fn main() {
     let mut register = 1;
-    let mut cycle = 1;
-    let mut results = Vec::new();
+    let mut cycle_nr = 0;
+    let mut pickles = vec![Vec::new(); SCREEN_HEIGHT as usize];
 
     let text = fs::read_to_string("input").unwrap();
     for instruction in text.trim().lines() {
-        next_cycle(register, &mut cycle, &mut results);
+        next_cycle(register, &mut cycle_nr, &mut pickles);
 
         match instruction.split_whitespace().collect::<Vec<_>>()[..] {
             ["noop"] => (),
             ["addx", val] => {
-                next_cycle(register, &mut cycle, &mut results);
+                next_cycle(register, &mut cycle_nr, &mut pickles);
                 register += val.parse::<isize>().unwrap();
             }
             _ => panic!("Unknown instruction"),
         }
     }
 
-    dbg!(results.iter().sum::<isize>());
+    println![
+        "{}",
+        pickles
+            .iter()
+            .map(|x| x.iter().collect::<String>())
+            .collect::<Vec<_>>()
+            .join("\n")
+    ];
 }
