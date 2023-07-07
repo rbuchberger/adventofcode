33131f599e6a678c2d006cd50adaadcd5f74c7ff
Robert Buchberger <robert@buchberger.cc>
2023-07-07 23:04:44 +0200

    Day 13, part 2 complete

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
-enum Value {
-    Integer(i8),
-    List(Vec<Value>),
+enum Val {
+    I(i8),
+    L(Vec<Val>),
 }
 
-impl Value {
+impl Val {
     fn parse(s: &str) -> (Self, usize) {
         let mut list = Vec::new();
         let mut current_int = String::new();
@@ -22,7 +22,7 @@ impl Value {
                 '0'..='9' => current_int.push(c),
                 ',' => {
                     if !current_int.is_empty() {
-                        list.push(Value::Integer(current_int.parse().unwrap()));
+                        list.push(Val::I(current_int.parse().unwrap()));
                         current_int.clear();
                     };
                 }
@@ -33,7 +33,7 @@ impl Value {
                 }
                 ']' => {
                     if !current_int.is_empty() {
-                        list.push(Value::Integer(current_int.parse().unwrap()));
+                        list.push(Val::I(current_int.parse().unwrap()));
                     }
                     length = i;
                     break;
@@ -42,41 +42,67 @@ impl Value {
             };
         }
 
-        (Value::List(list), length)
+        (Val::L(list), length)
     }
 }
 
-impl From<&str> for Value {
+impl From<&str> for Val {
     fn from(s: &str) -> Self {
         Self::parse(s).0
     }
 }
 
-impl PartialOrd for Value {
+impl PartialOrd for Val {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         match (self, other) {
-            (&Value::Integer(a), &Value::Integer(b)) => Some(i8::cmp(&a, &b)),
-            (Value::List(a), Value::List(b)) => a.partial_cmp(b),
-            (Value::Integer(_), Value::List(_)) => {
-                Value::List(vec![self.clone()]).partial_cmp(other)
-            }
-            (Value::List(_), Value::Integer(_)) => {
-                self.partial_cmp(&Value::List(vec![other.clone()]))
-            }
+            (&Val::I(a), &Val::I(b)) => Some(i8::cmp(&a, &b)),
+            (Val::L(a), Val::L(b)) => a.partial_cmp(b),
+            (Val::I(_), Val::L(_)) => Val::L(vec![self.clone()]).partial_cmp(other),
+            (Val::L(_), Val::I(_)) => self.partial_cmp(&Val::L(vec![other.clone()])),
         }
+    }
+}
+
+impl Ord for Val {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        println!("cmp {:?} and {:?}", self, other);
+        self.partial_cmp(other).unwrap()
     }
 }
 
 fn main() {
     let text = fs::read_to_string("input").unwrap();
 
-    let result: usize = text
+    let result1: usize = text
         .split("\n\n")
-        .map(|p| p.lines().map(|l| l.into()).collect::<Vec<Value>>())
+        .map(|p| p.lines().map(|l| l.into()).collect::<Vec<Val>>())
         .enumerate()
         .filter(|(_, p)| p[0] <= p[1])
         .map(|(i, _)| i + 1) // not zero indexed!
         .sum();
 
-    println!("Result: {}", result);
+    println!("Part 1 result: {}", result1);
+
+    let dividers = vec![
+        Val::L(vec![Val::L(vec![Val::I(2)])]),
+        Val::L(vec![Val::L(vec![Val::I(6)])]),
+    ];
+
+    let mut packets2 = text
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| l.into())
+        .collect::<Vec<Val>>();
+
+    packets2.append(&mut dividers.clone());
+    packets2.sort();
+
+    let result2: usize = packets2
+        .iter()
+        .enumerate()
+        .filter(|(_, p)| dividers.contains(p))
+        .map(|(i, _)| i + 1)
+        .product();
+
+    println!("Part 2 result: {:?}", result2);
 }
