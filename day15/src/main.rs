1e807319e2233052fd955be56840fb2649d7d2cf
Robert Buchberger <robert@buchberger.cc>
2023-07-12 17:49:48 +0200

    Day 15, part 2 complete
    Also refactored part 1 in order to reuse code for part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 use std::ops::RangeInclusive;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Pair {
     sensor: (isize, isize),
     beacon: (isize, isize),
@@ -45,12 +45,17 @@ impl Pair {
     }
 }
 
-fn part1(filename: &str, row: isize) -> isize {
-    let text = std::fs::read_to_string(filename).unwrap();
-
-    let mut coverage = text
+fn pairs(filename: &str) -> Vec<Pair> {
+    std::fs::read_to_string(filename)
+        .unwrap()
         .lines()
         .map(Pair::from)
+        .collect()
+}
+
+fn map_coverage(pairs: Vec<Pair>, row: isize) -> Vec<RangeInclusive<isize>> {
+    let mut coverage = pairs
+        .iter()
         .filter_map(|pair| pair.row_coverage(row))
         .collect::<Vec<_>>();
 
@@ -60,7 +65,10 @@ fn part1(filename: &str, row: isize) -> isize {
         .iter()
         .fold(Vec::new(), |mut acc: Vec<RangeInclusive<isize>>, curr| {
             if let Some(prev) = acc.last() {
-                match (prev.contains(curr.start()), prev.contains(curr.end())) {
+                match (
+                    (&(prev.end() + 1) >= curr.start()),
+                    prev.contains(curr.end()),
+                ) {
                     (false, false) => acc.push(curr.clone()), // new, separate section
                     (true, true) => (),                       // completely contained
                     (false, true) => unreachable!(),          // we already sorted by start
@@ -80,10 +88,36 @@ fn part1(filename: &str, row: isize) -> isize {
             acc
         });
 
+    coverage
+}
+
+fn part1(filename: &str, row: isize) -> isize {
+    let coverage = map_coverage(pairs(filename), row);
+
     coverage.iter().map(|r| r.end() - r.start()).sum()
+}
+
+const TUNING_FREQ: isize = 4_000_000;
+
+fn part2(filename: &str, bound: isize) -> isize {
+    let pairs = pairs(filename);
+
+    let (y, x) = (0..=bound)
+        .into_iter()
+        .map(|row| map_coverage(pairs.clone(), row))
+        .enumerate()
+        .find(|(_, coverage)| {
+            coverage.len() > 1 || coverage[0].start() > &0 || coverage[0].end() < &bound
+        })
+        .unwrap();
+
+    ((x[0].end() + 1) * TUNING_FREQ) + y as isize
 }
 
 fn main() {
     dbg!(part1("example", 10));
     dbg!(part1("input", 2_000_000));
+
+    dbg!(part2("example", 20));
+    dbg!(part2("input", 4_000_000));
 }
